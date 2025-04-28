a26033c222b96cafda1d124be62433ffdf146af0
mathieu domichard <mathieu.domichard@gmail.com>
2025-04-28 11:26:03 +0200

    chore: initial project setup with Spring Boot and Dockerized PostgreSQL

---
+++ src/main/java/com/frequencies/frequencies_tombola_api/FrequenciesTombolaApiApplication.java
@@ -0,0 +1,13 @@
+package com.frequencies.frequencies_tombola_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrequenciesTombolaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrequenciesTombolaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/frequencies/frequencies_tombola_api/FrequenciesTombolaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.frequencies.frequencies_tombola_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrequenciesTombolaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

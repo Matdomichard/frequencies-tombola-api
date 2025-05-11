252cd93ca2e8b17c06e0440cdcc3b4292e44e504
mathieu domichard <mathieu.domichard@gmail.com>
2025-05-12 00:00:03 +0200

    Obsolete files, including LotteryController, LotteryService, TicketController, StatsController, and HelloAssoFormIdController, were successfully removed from the project. The application now relies on updated components, and all changes were made without errors.

--- src/main/java/com/frequencies/tombola/service/LotteryService.java
+++
@@ -1,9 +0,0 @@
-package com.frequencies.tombola.service;
-
-import com.frequencies.tombola.dto.TicketDto;
-
-import java.util.Optional;
-
-public interface LotteryService {
-    Optional<TicketDto> drawWinner();
-}
--- src/main/java/com/frequencies/tombola/service/StatsService.java
+++
@@ -1,7 +0,0 @@
-package com.frequencies.tombola.service;
-
-import com.frequencies.tombola.dto.StatsDto;
-
-public interface StatsService {
-    StatsDto getStats();
-}
